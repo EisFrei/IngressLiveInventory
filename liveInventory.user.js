9fe41007fcaf7df0ee90b72978bd9a03f8cfd222
Martin <martin.freisen@gmail.com>
2021-02-10 13:09:32 +0100

    add key icon layer

--- liveInventory.user.js
+++ liveInventory.user.js
@@ -2,7 +2,7 @@
 // @id liveInventory
 // @name IITC Plugin: Live Inventory
 // @category Info
-// @version 0.0.1
+// @version 0.0.2
 // @namespace	https://github.com/EisFrei/IngressLiveInventory
 // @downloadURL	https://github.com/EisFrei/IngressLiveInventory/raw/main/liveInventory.user.js
 // @homepageURL	https://github.com/EisFrei/IngressLiveInventory
@@ -15,22 +15,22 @@
 
 function wrapper(plugin_info) {
 
-	// Make sure that window.plugin exists. IITC defines it as a no-op function,
-	// and other plugins assume the same.
-	if (typeof window.plugin !== "function") window.plugin = function () {};
-	const KEY_SETTINGS = "plugin-live-inventory";
+    // Make sure that window.plugin exists. IITC defines it as a no-op function,
+    // and other plugins assume the same.
+    if (typeof window.plugin !== "function") window.plugin = function () {};
+    const KEY_SETTINGS = "plugin-live-inventory";
 
-	window.plugin.LiveInventory = function () {};
+    window.plugin.LiveInventory = function () {};
 
-	const thisPlugin = window.plugin.LiveInventory;
-	// Name of the IITC build for first-party plugins
-	plugin_info.buildName = "LiveInventory";
+    const thisPlugin = window.plugin.LiveInventory;
+    // Name of the IITC build for first-party plugins
+    plugin_info.buildName = "LiveInventory";
 
-	// Datetime-derived version of the plugin
-	plugin_info.dateTimeVersion = "202102100950";
+    // Datetime-derived version of the plugin
+    plugin_info.dateTimeVersion = "202102100950";
 
-	// ID/name of the plugin
-	plugin_info.pluginId = "liveInventory";
+    // ID/name of the plugin
+    plugin_info.pluginId = "liveInventory";
 
     const translations = {
         BOOSTED_POWER_CUBE: 'Hypercube',
@@ -96,6 +96,25 @@ function wrapper(plugin_info) {
         }
     }
 
+    function svgToIcon(str, s) {
+        const url = ("data:image/svg+xml," + encodeURIComponent(str)).replace(/#/g, '%23');
+        return new L.Icon({
+            iconUrl: url,
+            iconSize: [s, s],
+            iconAnchor: [s / 2, s / 2],
+            className: 'no-pointer-events', //allows users to click on portal under the unique marker
+        })
+    }
+
+    function createIcons() {
+        thisPlugin.keyIcon = svgToIcon(`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-key" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+<circle cx="8" cy="15" r="4" />
+<line x1="10.85" y1="12.15" x2="19" y2="4" />
+<line x1="18" y1="5" x2="20" y2="7" />
+<line x1="15" y1="8" x2="17" y2="10" />
+</svg>`, 15);
+    }
+
     function prepareItemCounts(data) {
         if (!data || !data.result) {
             return [];
@@ -153,7 +172,7 @@ function wrapper(plugin_info) {
             addKeyToCount(item, countMap, 1);
             if (item[2].container) {
                 item[2].container.stackableItems.forEach((item2) => {
-                    addKeyToCount(item2.exampleGameEntity, countMap, item2.itemGuids.length,  item[2].moniker.differentiator);
+                    addKeyToCount(item2.exampleGameEntity, countMap, item2.itemGuids.length, item[2].moniker.differentiator);
                 });
             }
         });
@@ -167,7 +186,7 @@ function wrapper(plugin_info) {
         return countList;
     }
 
-    function displayInventory () {
+    function displayInventory() {
         dialog({
             html: `<div id="live-inventory">
 <table>
@@ -233,21 +252,21 @@ ${thisPlugin.keyCount.map((el)=> {
     function loadInventory() {
         try {
             const localData = JSON.parse(localStorage[KEY_SETTINGS]);
-            if (localData && localData.expires > Date.now()){
+            if (localData && localData.expires > Date.now()) {
                 prepareData(localData.data);
                 return;
             }
-        } catch(e){}
+        } catch (e) {}
 
         window.postAjax('getInventory', {
             "lastQueryTimestamp": 0
-        }, (data,textStatus,jqXHR) => {
+        }, (data, textStatus, jqXHR) => {
             localStorage[KEY_SETTINGS] = JSON.stringify({
                 data: data,
                 expires: Date.now() + 5 * 60 * 1000 // request data only once per five minutes, or we might hit a rate limit
             });
             prepareData(data);
-        }, (data,textStatus,jqXHR) => {
+        }, (data, textStatus, jqXHR) => {
             console.error(data);
         });
     };
@@ -263,6 +282,44 @@ ${thisPlugin.keyCount.map((el)=> {
         }
     }
 
+    function addKeyToLayer(data) {
+        const tileParams = window.getCurrentZoomTileParameters ? window.getCurrentZoomTileParameters() : window.getMapZoomTileParameters();
+        if (tileParams.level !== 0) {
+            return;
+        }
+
+        if (thisPlugin.keyMap[data.portal.options.guid] && !data.portal._keyMarker) {
+            data.portal._keyMarker = L.marker(data.portal._latlng, {
+                icon: thisPlugin.keyIcon,
+                interactive: false,
+                keyboard: false,
+            }).addTo(thisPlugin.layerGroup);
+        }
+    }
+
+    function removeKeyFromLayer(data) {
+        if (data.portal._keyMarker) {
+            thisPlugin.layerGroup.removeLayer(data.portal._keyMarker);
+            delete data.portal._keyMarker;
+        }
+    }
+
+    function checkShowAllIcons(data) {
+        const tileParams = window.getCurrentZoomTileParameters ? window.getCurrentZoomTileParameters() : window.getMapZoomTileParameters();
+        if (tileParams.level !== 0) {
+            thisPlugin.layerGroup.clearLayers();
+            for (let id in window.portals) {
+                delete window.portals[id]._keyMarker;
+            }
+        } else {
+            for (let id in window.portals) {
+                addKeyToLayer({
+                    portal: window.portals[id]
+                });
+            }
+        }
+    }
+
     function setup() {
         loadInventory();
         $('<a href="#">')
@@ -270,41 +327,48 @@ ${thisPlugin.keyCount.map((el)=> {
             .click(displayInventory)
             .appendTo($('#toolbox'));
         window.addHook('portalDetailsUpdated', portalDetailsUpdated);
+        window.addHook('portalAdded', addKeyToLayer);
+        window.addHook('portalRemoved', removeKeyFromLayer);
+        window.map.on('zoom', checkShowAllIcons);
     }
 
-    function delaySetup(){
-        setTimeout(setup, 5000); // delay setup and thus requesting data, or we might encounter a server error
-    }
-	delaySetup.info = plugin_info; //add the script info data to the function as a property
+    function delaySetup() {
+        thisPlugin.layerGroup = new L.LayerGroup();
+        window.addLayerGroup('Portal keys', thisPlugin.layerGroup, false);
+        createIcons();
 
-	if (window.iitcLoaded) {
-		delaySetup();
-	} else {
-		if (!window.bootPlugins) {
-			window.bootPlugins = [];
-		}
-		window.bootPlugins.push(delaySetup);
-	}
+        setTimeout(setup, 1000); // delay setup and thus requesting data, or we might encounter a server error
+    }
+    delaySetup.info = plugin_info; //add the script info data to the function as a property
+
+    if (window.iitcLoaded) {
+        delaySetup();
+    } else {
+        if (!window.bootPlugins) {
+            window.bootPlugins = [];
+        }
+        window.bootPlugins.push(delaySetup);
+    }
 }
 
 
 (function () {
-	const plugin_info = {};
-	if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
-		plugin_info.script = {
-			version: GM_info.script.version,
-			name: GM_info.script.name,
-			description: GM_info.script.description
-		};
-	}
-	// Greasemonkey. It will be quite hard to debug
-	if (typeof unsafeWindow != 'undefined' || typeof GM_info == 'undefined' || GM_info.scriptHandler != 'Tampermonkey') {
-		// inject code into site context
-		const script = document.createElement('script');
-		script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(plugin_info) + ');'));
-		(document.body || document.head || document.documentElement).appendChild(script);
-	} else {
-		// Tampermonkey, run code directly
-		wrapper(plugin_info);
-	}
+    const plugin_info = {};
+    if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) {
+        plugin_info.script = {
+            version: GM_info.script.version,
+            name: GM_info.script.name,
+            description: GM_info.script.description
+        };
+    }
+    // Greasemonkey. It will be quite hard to debug
+    if (typeof unsafeWindow != 'undefined' || typeof GM_info == 'undefined' || GM_info.scriptHandler != 'Tampermonkey') {
+        // inject code into site context
+        const script = document.createElement('script');
+        script.appendChild(document.createTextNode('(' + wrapper + ')(' + JSON.stringify(plugin_info) + ');'));
+        (document.body || document.head || document.documentElement).appendChild(script);
+    } else {
+        // Tampermonkey, run code directly
+        wrapper(plugin_info);
+    }
 })();
